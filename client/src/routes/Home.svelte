59c5046aff10a57ce9100264501049aeac296ffa
Joey Li <joey@joeyli.dev>
2024-05-10 23:51:11 +0200

    boing

---
+++ client/src/routes/Home.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import Editor from "../lib/Editor.svelte";
+
+  let value: string = "Poephoofd";
+
+  const onSave = () => {
+    alert(value);
+  }
+</script>
+
+<section class="home">
+  <Editor bind:value mode="tabs" />
+  <div class="button-wrapper">
+    <button on:click={onSave}>Save</button>
+  </div>
+</section>
+
+<style lang="scss">
+    .home {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        height: 100vh;
+        width: 100vw;
+        padding: 2rem;
+    }
+
+    .button-wrapper {
+        display: flex;
+        justify-content: flex-end;
+        width: 100% ;
+    }
+</style>
