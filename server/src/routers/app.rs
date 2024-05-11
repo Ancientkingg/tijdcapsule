531af92d8bcb422d4a00d114430ab417bc1cf9bf
Samuel Bruin <S.bruin-1@student.tudelft.nl>
2024-05-11 21:28:41 +0200

    Add CORS and TLS  🔒

--- server/src/routers/app.rs
+++ server/src/routers/app.rs
@@ -1,18 +1,27 @@
 
 use std::net::SocketAddr;
 
-use axum::{routing::{ post, get }, Router};
+use axum::{http, routing::{ get, post }, Router};
 use axum_client_ip::SecureClientIpSource;
+use tower_http::cors::{Any, CorsLayer};
+use http::Method;
 
 use super::root;
 use super::capsule;
 
+
+
 fn new() -> Router {
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST])
+        .allow_origin(Any);
+
     Router::new()
         .route("/", get(root::handler::get))
         .route("/capsule", post(capsule::handler::post))
         .route("/capsule/:capsule_id", get(capsule::handler::get))
         .layer(SecureClientIpSource::ConnectInfo.into_extension())
+        .layer(cors)
 }
 
 pub async fn init(addr: &'static str) {
